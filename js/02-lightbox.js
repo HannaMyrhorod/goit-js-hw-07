7fd5e8c23ff4614c625bf392ee385e1d991caacd
HannaMyrhorod <hanna.mirgorod@gmail.com>
2022-05-27 21:29:20 +0300

    Add Task #2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+insertGalleryMarkup(createGalleryMarkup(galleryItems));
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.reduce((acc, { preview, original, description }) => acc +
+        `<li><a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a></li>`, "");
+};
+
+function insertGalleryMarkup(markup) {
+    gallery.insertAdjacentHTML('beforeend', markup);
+};
